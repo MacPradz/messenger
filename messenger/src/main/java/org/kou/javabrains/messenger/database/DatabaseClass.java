23ebd8af995c6d0cb3ce7953306ad6ccf4635042
MacPradz <macpradz@gmail.com>
2016-06-08 17:15:25 +0200

    added ProfileResource and ProfileService

--- messenger/src/main/java/org/kou/javabrains/messenger/database/DatabaseClass.java
+++ messenger/src/main/java/org/kou/javabrains/messenger/database/DatabaseClass.java
@@ -4,16 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.kou.javabrains.messenger.model.Message;
+import org.kou.javabrains.messenger.model.Profile;
 
 public class DatabaseClass {
 	private static Map<Long, Message> messages = new HashMap<>();
-	private static Map<Long, Message> profiles = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
 
 	public static Map<Long, Message> getMessages() {
 		return messages;
 	}
 
-	public static Map<Long, Message> getProfiles() {
+	public static Map<String, Profile> getProfiles() {
 		return profiles;
 	}
 
---
+++ messenger/src/main/java/org/kou/javabrains/messenger/service/ProfileService.java
@@ -0,0 +1,42 @@
+package org.kou.javabrains.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.kou.javabrains.messenger.database.DatabaseClass;
+import org.kou.javabrains.messenger.model.Profile;
+
+public class ProfileService {
+	private static Map<String, Profile> profiles = DatabaseClass.getProfiles();
+
+	public ProfileService() {
+		profiles.put("maciej", new Profile(1L, "maciej", "Maciej", "Pradzynski"));
+	}
+
+	public List<Profile> getAllProfiles() {
+		return new ArrayList<>(profiles.values());
+	}
+
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+	}
+
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile updateProfile(Profile profile) {
+		if (profile.getProfileName().isEmpty()) {
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile removeProfile(String profileName) {
+		return profiles.remove(profileName);
+	}
+}
