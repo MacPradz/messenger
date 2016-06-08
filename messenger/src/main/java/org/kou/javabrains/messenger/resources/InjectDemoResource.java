475900607d4f877f46c221d1efa7ccf41e6a7c8d
MacPradz <macpradz@gmail.com>
2016-06-08 18:45:11 +0200

    added more annotations in InjectDemoResource

--- messenger/src/main/java/org/kou/javabrains/messenger/resources/InjectDemoResource.java
+++ messenger/src/main/java/org/kou/javabrains/messenger/resources/InjectDemoResource.java
@@ -1,21 +1,41 @@
 package org.kou.javabrains.messenger.resources;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/injectdemo") 
-@Consumes(MediaType.TEXT_PLAIN) 
-@Produces(MediaType.TEXT_PLAIN) 
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
 public class InjectDemoResource {
 
 	@GET
-	@Path("annotations")
-	//MatrigParam is almost the same as QueryParam but here you access .../injectdemo/annotations;param=value
-	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam) {
+	@Path("annoMatrixParam")
+	// MatrigParam is almost the same as QueryParam but here you access
+	// .../injectdemo/annoMatrixParam;param=value
+	public String getParamsUsingMatrixParam(@MatrixParam("param") String matrixParam) {
 		return matrixParam;
 	}
+
+	@GET
+	@Path("annoHeaderParam")
+	// to get headers
+	public String getParamsUsingHeaderParam(@HeaderParam("customHeader") String header) {
+		return header;
+	}
+
+	@GET
+	@Path("annoCookieParam")
+	// to try this in Postman create header with name "Cookie" and as value set
+	// i.e. "name=value" which will create cookie "name" with value "value"
+	public String getParamsUsingCookieParam(@CookieParam("name") String cookie) {
+		return cookie;
+	}
+
+	// @FormParam for submitting html forms
 }
